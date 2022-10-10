112678dcde42c71f5a9fa25c3439613d2503ca27
lishiwen <lishiwen@testor.com.cn>
2022-10-10 15:09:15 +0800

    函数的返回是函数

--- src/main/java/com/lsw/scala/func/ShowFunc.scala
+++ src/main/java/com/lsw/scala/func/ShowFunc.scala
@@ -1,5 +1,7 @@
 package com.lsw.scala.func
 
+import java.util.Date
+
 object ShowFunc {
 
 
@@ -19,9 +21,20 @@ object ShowFunc {
     func2(1, 2, 3)
 
     //匿名函数 =>
-    ((x:Int)=>{
+    ((x: Int) => {
       println(s"\n匿名函数$x")
-    })(1)
+    }) (1)
+
+    //偏应用函数
+    func3(new Date(), "func3 abc")
+    func4("func4 abc")
+
+    //高阶函数
+
+    //函数的参数 是函数
+    func5((x:Int,y:Int) => {x+y}, "abc")
+    //函数的返回是函数
+   println( func6()("lisi","110"))
   }
 
   /**
@@ -99,8 +112,41 @@ object ShowFunc {
     for (ele <- x) {
       println(ele)
     }
-    x.foreach((ele:Int)=>{print(ele)})
+    x.foreach((ele: Int) => {
+      print(ele)
+    })
     x.foreach(print)
     x.foreach(print(_))
+  }
+
+  //偏应用函数
+  def func3(date: Date, log: String): Unit = {
+    println(s"time $date $log")
+  }
+
+  //偏应用函数 当函数参数过多时 重复操作时使用  _ 传需要传的
+  def func4 = func3(new Date(), _: String)
+
+  /**
+   * 函数的参数 是函数
+   *
+   * @param x
+   * @param y
+   */
+  def func5(x: (Int,Int) => Int, y: String): Unit = {
+    var str: Int = x(10,20)
+    println(str, y)
+  }
+
+  /**
+   * 函数的返回是函数
+   * @param x
+   * @param y
+   */
+  def func6(): (String,String)=>String = {
+    def a(name:String,phone:String): String ={
+      return s"$name is phone number  $phone"
+    }
+    return a
   }
 }
